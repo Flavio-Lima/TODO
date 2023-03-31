750ce51dfe9e34ca264e8cafce47617ff892f190
Flavio Lima <flavio.lima981@gmail.com>
2023-03-31 09:25:51 -0300

    adiciona fase inical do projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => (
+  <section id='app' className='container'>
+    <header></header>
+    <h1 className='title'>TODO</h1>
+    <input />
+  </section>
+
+);
+
+export default App;
