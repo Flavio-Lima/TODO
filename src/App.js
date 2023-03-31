dc509ca1a7e115cdee16c5a02aa3b9711058ccc0
Flavio Lima <flavio.lima981@gmail.com>
2023-03-31 10:16:02 -0300

    cria entrada de tarefas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 
-const App = () => (
-  <section id='app' className='container'>
-    <header></header>
-    <h1 className='title'>TODO</h1>
-    <input />
-  </section>
+const App = () => {
 
-);
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
 
+  const [value, setValue] = useState('')
+
+  const erase = () => {
+    setValue('')
+  }
+
+  const submit = () => {
+    console.log(value);
+    erase();
+  }
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const onKeyDown = (event) => {
+    if(event.which === ENTER_KEY)
+    {
+      submit();
+    }
+    else if (event.which === ESCAPE_KEY)
+    {
+      erase();
+    }
+  }
+
+  return(
+    <section id='app' className='container'>
+      <header>
+        <h1 className='title'>TODO</h1>
+      </header>
+
+      <section className='main'>
+        <input
+        className='new-todo'
+        placeholder='O que precisa ser feito?'
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        />
+      </section>
+    </section>
+
+  );
+}
 export default App;
